323381327bc25137e31a6f3bae8dc6a865d842ec
Zirmith <f2v74yt@gmail.com>
2023-06-24 21:20:35 -0500

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Array to store HWIDs
+const hwidArray = [];
+
+// Route to get all HWIDs
+app.get('/api/hwids', (req, res) => {
+  res.json({ hwids: hwidArray });
+});
+
+// Route to add a new HWID
+app.post('/api/hwids', (req, res) => {
+  const { hwid } = req.body;
+
+  if (hwid && !hwidArray.includes(hwid)) {
+    hwidArray.push(hwid);
+    res.json({ message: 'HWID added successfully.' });
+  } else {
+    res.status(400).json({ error: 'Invalid HWID or HWID already exists.' });
+  }
+});
+
+// Route to whitelist a HWID
+app.put('/api/hwids/whitelist', (req, res) => {
+  const { hwid } = req.body;
+
+  if (hwid && hwidArray.includes(hwid)) {
+    // Remove the HWID from the array if it's already blacklisted
+    const index = hwidArray.indexOf(hwid);
+    if (index > -1) {
+      hwidArray.splice(index, 1);
+    }
+
+    res.json({ message: 'HWID whitelisted successfully.' });
+  } else {
+    res.status(400).json({ error: 'Invalid HWID or HWID not found.' });
+  }
+});
+
+// Route to blacklist a HWID
+app.put('/api/hwids/blacklist', (req, res) => {
+  const { hwid } = req.body;
+
+  if (hwid && hwidArray.includes(hwid)) {
+    // Remove the HWID from the array if it's already whitelisted
+    const index = hwidArray.indexOf(hwid);
+    if (index > -1) {
+      hwidArray.splice(index, 1);
+    }
+
+    res.json({ message: 'HWID blacklisted successfully.' });
+  } else {
+    res.status(400).json({ error: 'Invalid HWID or HWID not found.' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Luminous API is running on http://localhost:${port}`);
+});
