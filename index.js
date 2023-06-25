9692914b9082bb228bba066fce7fc95eac347902
Zirmith <f2v74yt@gmail.com>
2023-06-24 21:55:48 -0500

    initial com

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 3000;
+
+
+app.use(cors());
+app.use(express.json());
 
 // Array to store HWIDs
 const hwidArray = [];
