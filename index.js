af52299832e458c0abd4687e3b3f1f0d15e9d690
ZDLua <hj_jr@icloud.com>
2023-06-29 07:51:44 -0500

    r1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const fetch = require('fetch');
 const app = express();
 const port = 3000;
 
